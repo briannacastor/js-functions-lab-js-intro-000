77ab9f08c8ed83931b636e35d0e18190fdb99951
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-09-26 00:34:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var holiday="Christmas"
 var days="10"
 
 function happyHolidays(){
-  return 'Happy Holidays!'
+  console.log("Happy Holidays!")
 }
 function happyHolidaysTo(){
   return `Happy holiday,${name}!`
