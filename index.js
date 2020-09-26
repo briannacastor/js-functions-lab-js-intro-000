41eaa0e59e0221ff8bb3009dac1263c816aa19b7
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-09-26 00:16:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function happyHolidays(string){
   console.log("Happy holidays!")
 }
 function happyHolidaysTo(name){
-  console.log (`Happy holidays+ ${name}!`)
+  console.log ("Happy holidays"+name)
 }
 function happyHolidaysTo(name, holiday){
-  console.log(`Happy Holidays,+${holiday},+${name}+!`)
+  console.log("Hap")
 }
 function holidayCountdown(holiday, days){
   return `It\'s ${days} days until ${holiday}!`
